279a22c3948587d556ec18f70251d61928317d99
David Walsh <davidwalsh83@gmail.com>
2016-01-21 15:11:04 -0600

    Restore main plugin file

---
+++ wp-sw-cache/wp-sw-cache.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: WordPress ServiceWorker Cache
+Plugin URI: https://github.com/darkwing/wp-sw-cache
+Description: This WordPress plugin provides a method for caching theme assets via a service worker.
+Version: 0.1-alpha
+Text Domain: wpswcache
+Author: David Walsh
+Author URI:  https://davidwalsh.name
+*/
+
+require_once(plugin_dir_path(__FILE__) . 'wp-sw-cache-main.php');
+require_once(plugin_dir_path(__FILE__) . 'wp-sw-cache-db.php');
+
+SW_Cache_DB::init();
+
+if (is_admin()) {
+  require_once(plugin_dir_path(__FILE__) . 'wp-sw-cache-admin.php');
+  SW_Cache_DB::init();
+}
+
+register_activation_hook(__FILE__, array('SW_Cache_DB', 'on_activate'));
+register_deactivation_hook(__FILE__, array('SW_Cache_DB', 'on_deactivate'));
+register_uninstall_hook(__FILE__, array('SW_Cache_DB', 'on_uninstall'));
+
+?>
