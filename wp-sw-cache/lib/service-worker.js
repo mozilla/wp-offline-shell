e4afdac33ec0be10a634c600da343119d420e63f
Salvador de la Puente González <salva@unoyunodiez.com>
2016-02-18 18:13:02 +0100

    Adding compatibility with WP SW Manager

--- wp-sw-cache/lib/service-worker.js
+++ wp-sw-cache/lib/service-worker.js
@@ -1,61 +1,65 @@
-var CACHE_NAME = '$name';
+(function(self){
+  var CACHE_PREFIX = '__wp-sw-cache::';
 
-var CACHE_FILES = $files;
+  var CACHE_NAME = CACHE_PREFIX + $name;
 
-self.addEventListener('install', function(event) {
-  // Perform install step:  loading each required file into cache
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        // Add all offline dependencies to the cache
-        console.log('[install] Caches opened, adding all core components' +
-          'to cache');
-        return cache.addAll(CACHE_FILES);
-      })
-      .then(function() {
-        console.log('[install] All required resources have been cached, ' +
-          'we\'re good!');
-        return self.skipWaiting();
-      })
-  );
-});
+  var CACHE_FILES = $files;
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return the response from the cached version
-        if (response) {
-          console.log(
-            '[fetch] Returning from ServiceWorker cache: ',
-            event.request.url
-          );
-          return response;
-        }
-        // Not in cache - return the result from the live server
-        // `fetch` is essentially a "fallback"
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('[activate] Activating ServiceWorker!');
-
-  // Clean up old cache in the background
-  caches.keys().then(function(cacheNames) {
-    return Promise.all(
-      cacheNames.map(function(cacheName) {
-        if(cacheName != CACHE_NAME) {
-          console.log('[activate] Deleting out of date cache:', cacheName);
-          return caches.delete(cacheName);
-        }
-      })
+  self.addEventListener('install', function(event) {
+    // Perform install step:  loading each required file into cache
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          // Add all offline dependencies to the cache
+          console.log('[install] Caches opened, adding all core components' +
+            'to cache');
+          return cache.addAll(CACHE_FILES);
+        })
+        .then(function() {
+          console.log('[install] All required resources have been cached, ' +
+            'we\'re good!');
+          return self.skipWaiting();
+        })
     );
   });
 
-  // Calling claim() to force a "controllerchange" event on navigator.serviceWorker
-  console.log('[activate] Claiming this ServiceWorker!');
-  event.waitUntil(self.clients.claim());
-});
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return the response from the cached version
+          if (response) {
+            console.log(
+              '[fetch] Returning from ServiceWorker cache: ',
+              event.request.url
+            );
+            return response;
+          }
+          // Not in cache - return the result from the live server
+          // `fetch` is essentially a "fallback"
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+
+  self.addEventListener('activate', function(event) {
+    console.log('[activate] Activating ServiceWorker!');
+
+    // Clean up old cache in the background
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if(cacheName.startsWith(CACHE_PREFIX) && cacheName != CACHE_NAME) {
+            console.log('[activate] Deleting out of date cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    });
+
+    // Calling claim() to force a "controllerchange" event on navigator.serviceWorker
+    console.log('[activate] Claiming this ServiceWorker!');
+    event.waitUntil(self.clients.claim());
+  });
+})(self);
--- wp-sw-cache/wp-sw-cache-main.php
+++ wp-sw-cache/wp-sw-cache-main.php
@@ -2,6 +2,7 @@
 
 require_once(plugin_dir_path(__FILE__).'wp-sw-cache.php' );
 require_once(plugin_dir_path(__FILE__).'wp-sw-cache-db.php');
+require_once(plugin_dir_path(__FILE__).'vendor/mozilla/wp-sw-manager/class-wp-sw-manager.php');
 
 load_plugin_textdomain('wpswcache', false, dirname(plugin_basename(__FILE__)) . '/lang');
 
@@ -9,11 +10,7 @@ class SW_Cache_Main {
   private static $instance;
 
   public function __construct() {
-    if($this->show_header()) {
-      add_action('wp_head', array($this, 'register')); // TODO:  Find out why "wp_footer" isn't working
-    }
-
-    add_action('parse_request', array($this, 'on_parse_request'));
+    WP_SW_Manager::get_manager()->sw()->add_content(array($this, "write_sw"));
   }
 
   public static function init() {
@@ -22,43 +19,19 @@ class SW_Cache_Main {
     }
   }
 
-  public function show_header() {
+  public function write_sw() {
     $files = get_option('wp_sw_cache_files');
     if(!$files) {
-      $files = array();
-    }
-
-    // TODO:  Make sure that files are all valid and exist
-    // I guess we can just leave out files that don't exist anymore
-
-    return get_option('wp_sw_cache_enabled') && count($files) && !is_admin();
-  }
-
-  public function register() {
-    $contents = file_get_contents(dirname(__FILE__).'/lib/service-worker-registration.html');
-    echo $contents;
-  }
-
-  public function on_parse_request() {
-    // TODO:  The relative path to the "wp-sw-cache-worker.js" file really needs to be dynamic, not hardcoded
-    // This is bad
-
-    $files = get_option('wp_sw_cache_files');
-    if(!$files) {
-      $files = array();
+        $files = array();
     }
     foreach($files as $index=>$file) {
-      $files[$index] = get_template_directory_uri().'/'.$file;
+        $files[$index] = get_template_directory_uri().'/'.$file;
     }
 
-    if($_SERVER['REQUEST_URI'] === '/wp-sw-cache-worker.js') {
-      header('Content-Type: application/javascript');
-      $contents = file_get_contents(dirname(__FILE__).'/lib/service-worker.js');
-      $contents = str_replace('$name', get_option('wp_sw_cache_name'), $contents);
-      $contents = str_replace('$files', json_encode($files), $contents);
-      echo $contents;
-      exit();
-    }
+    $contents = file_get_contents(dirname(__FILE__).'/lib/service-worker.js');
+    $contents = str_replace('$name', json_encode(get_option('wp_sw_cache_name')), $contents);
+    $contents = str_replace('$files', json_encode($files), $contents);
+    echo $contents;
   }
 }
 
