3fb68d6bab09ea48926ee666d461af3dcc574edc
David Walsh <davidwalsh83@gmail.com>
2016-02-23 15:52:34 -0600

    Tagging as 0.2.0

--- wp-sw-cache/wp-sw-cache.php
+++ wp-sw-cache/wp-sw-cache.php
@@ -3,7 +3,7 @@
 Plugin Name: WP ServiceWorker Cache
 Plugin URI: https://github.com/darkwing/wp-sw-cache
 Description: This WordPress plugin provides a method for caching theme assets via a service worker.
-Version: 0.1-alpha
+Version: 0.2
 Text Domain: wpswcache
 Author: David Walsh
 Author URI:  https://davidwalsh.name
