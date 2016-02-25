b12c628153722bbb4495a690eda90dcbad9730c6
David Walsh <davidwalsh83@gmail.com>
2016-02-25 12:39:32 -0600

    Add first round of tests for plugin

--- wp-sw-cache/wp-sw-cache-db.php
+++ wp-sw-cache/wp-sw-cache-db.php
@@ -11,15 +11,17 @@ class SW_Cache_DB {
     if (!self::$instance) {
       self::$instance = new self();
     }
-
     return self::$instance;
   }
 
   public static function on_activate() {
-    // Set default options.
+    // For v1 we'll prompt the user enable the plugin manually upon activation
     update_option('wp_sw_cache_enabled', false);
-    update_option('wp_sw_cache_files', array());
+    // The "style.css" file is a standard WordPress file, so we can safely assume this exists
+    update_option('wp_sw_cache_files', array('style.css'));
+    // Create an initial SW version
     SW_Cache_Main::update_version();
+    // Setting debug initially will help the user understand what the SW is doing via the console
     update_option('wp_sw_cache_debug', true);
   }
 
